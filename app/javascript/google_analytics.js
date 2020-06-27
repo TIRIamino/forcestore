0fe79dfea139e0fcff685796b58ac8eeba54768d
thierry vinagre <vinagre.thierry@yahoo.fr>
2020-06-27 18:50:38 +0200

    debug

--- app/javascript/google_analytics.js
+++ app/javascript/google_analytics.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbolinks:load', function(event) {
 if (typeof gtag === 'function') {
-gtag('config', GA=UA-105300836-8 , {
+gtag('config', {GA} , {
 'page_location': event.data.url
 });
 }
