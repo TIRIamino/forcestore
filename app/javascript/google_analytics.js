c0dcb70115b98316d7ed5c6a6003b5ade649642d
thierry vinagre <vinagre.thierry@yahoo.fr>
2020-06-27 18:43:26 +0200

    variable env configuration

--- app/javascript/google_analytics.js
+++ app/javascript/google_analytics.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbolinks:load', function(event) {
 if (typeof gtag === 'function') {
-gtag('config', GA , {
+gtag('config', GA=UA-105300836-8 , {
 'page_location': event.data.url
 });
 }
